174d6c8cf652d02da732d68dbfee571399eed761
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2017-09-17 18:45:06 -0300

    Desafio: Criando o cadastro de pessoa

---
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -0,0 +1,27 @@
+CREATE SEQUENCE pessoa_seq;
+
+CREATE TABLE pessoa (
+	codigo BIGINT PRIMARY KEY DEFAULT NEXTVAL ('pessoa_seq'),
+	nome VARCHAR(50) NOT NULL,
+	ativo boolean,
+	logradouro VARCHAR(250),
+	numero VARCHAR(50) NOT NULL,
+	complemento VARCHAR(50) NOT NULL,
+	bairro VARCHAR(50) NOT NULL,
+	cep VARCHAR(50) NOT NULL,
+	cidade VARCHAR(50) NOT NULL,
+	estado VARCHAR(50) NOT NULL
+	
+) ;
+
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Tiago',TRUE,'Rua 1','345','complemento teste 1','Montese','3456','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Carlos',TRUE,'Rua 2','345','complemento teste 2','Montese','2356','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Sara',TRUE,'Rua 3','345','complemento teste 3','Montese','1267','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Joao',TRUE,'Rua 4','345','complemento teste 4','Montese','4674','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Victor',TRUE,'Rua 5','345','complemento teste 4','Montese','8357','Fortaleza','CE');
+
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Ana',TRUE,'Rua 6','345','complemento teste 5','Montese','7894','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Jocasta',TRUE,'Rua 7','345','complemento teste 6','Montese','4821','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Juliana',TRUE,'Rua 7','345','complemento teste 7','Montese','4757','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Josias',TRUE,'Rua 1','345','complemento teste 8','Montese','8532','Fortaleza','CE');
+INSERT INTO pessoa (nome,ativo,logradouro,numero,complemento,bairro,cep,cidade,estado) values ('Eliene',TRUE,'Rua 1','345','complemento teste 9','Montese','4664','Fortaleza','CE');
